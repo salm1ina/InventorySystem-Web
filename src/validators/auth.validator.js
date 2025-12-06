97dd206d570afe5e2645fdd76c2144864e404902
salm1ina <nabilaakbars1@gmail.com>
2025-12-06 21:08:29 +0800

    feat(validators): add auth validators

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const registerSchema = Joi.object({
+  name: Joi.string().min(3).max(100).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .min(8)
+    .pattern(new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])'))
+    .required()
+    .messages({
+      'string.pattern.base': 'Password must include uppercase, lowercase, number, and special char'
+    }),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = { registerSchema, loginSchema };
